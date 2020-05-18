40752be571c30cd460beca53d5ae5e7293c68531
Beto Licks <betaolicks@gmail.com>
2020-05-18 09:49:15 -0300

    CRIACAO DE CONTROLLER E ENTIDADES

--- bookstore-api/src/main/java/com/bookstore/BookstoreApiApplication.java
+++ bookstore-api/src/main/java/com/bookstore/BookstoreApiApplication.java
@@ -2,8 +2,14 @@ package com.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.controller")
+@EntityScan("com.entidades")
+@EnableJpaRepositories
 public class BookstoreApiApplication {
 
 	public static void main(String[] args) {
